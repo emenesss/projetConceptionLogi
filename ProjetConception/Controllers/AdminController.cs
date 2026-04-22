5ad5cc647a173af3e3c1d8a5eec865ce3cbe61c2
Marc-André Grétry <magretry@outlook.com>
2026-04-22 00:34:54 -0400

    User management

--- ProjetConception/Controllers/AdminController.cs
+++ ProjetConception/Controllers/AdminController.cs
@@ -59,5 +59,84 @@ namespace ProjetConception.Controllers
 
             return View();
         }
+        
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            if (HttpContext.Session.GetString("Role") != "Admin")
+                return RedirectToAction("Index", "Client");
+
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult CreateUser(int id, string name, string username, string password, Role role)
+        {
+            if (HttpContext.Session.GetString("Role") != "Admin")
+                return RedirectToAction("Index", "Client");
+
+            if (string.IsNullOrWhiteSpace(name) ||
+                string.IsNullOrWhiteSpace(username) ||
+                string.IsNullOrWhiteSpace(password))
+            {
+                ViewBag.Error = "Tous les champs sont obligatoires.";
+                return View();
+            }
+
+            User user = new User(id, name, username, password, role);
+
+            userDao.InsertUser(user);
+
+            TempData["Message"] = "Utilisateur créé avec succès.";
+            return RedirectToAction("Users");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteUser(int id)
+        {
+            if (HttpContext.Session.GetString("Role") != "Admin")
+                return RedirectToAction("Index", "Client");
+
+            userDao.DeleteUser(id);
+
+            TempData["Message"] = "Utilisateur supprimé.";
+            return RedirectToAction("Users");
+        }
+
+        
+        [HttpGet]
+        public IActionResult EditUser(int id)
+        {
+            if (HttpContext.Session.GetString("Role") != "Admin")
+                return RedirectToAction("Index", "Client");
+
+            var user = userDao.SelectUserById(id);
+
+            if (user == null)
+                return RedirectToAction("Index");
+
+            return View(user);
+        }
+
+        
+        [HttpPost]
+        public IActionResult EditUser(int id, string name, string username, string password, Role role)
+        {
+            if (HttpContext.Session.GetString("Role") != "Admin")
+                return RedirectToAction("Index", "Client");
+
+            userDao.UpdateUserName(id, name);
+            userDao.UpdateUsername(id, username);
+
+            if (!string.IsNullOrWhiteSpace(password))
+                userDao.UpdatePassword(id, password);
+
+            userDao.UpdateRole(id, role);
+
+            TempData["Message"] = "Utilisateur modifié.";
+            return RedirectToAction("Users");
+        }
     }
 }
